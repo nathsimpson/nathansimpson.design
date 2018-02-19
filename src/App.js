84a76c39655f7d4a5830939284da8283247209e6
Nathan <nathan@nsgd.com.au>
2018-02-19 13:22:10 +1100

    Dynamically creating portfolio projects

--- src/App.js
+++ src/App.js
@@ -3,6 +3,27 @@ import logo from './logo.svg';
 import nathansnow from './nathansnow.jpg';
 import './App.css';
 
+var portfolioInfo = {
+  projects: [
+    {
+      projectName : "Simmo's Offroad Tours",
+      imagesrc : "nathansnow.jpg",
+      tags: ["brand", "ux", "dev"],
+    },{
+      projectName : "ITC Learning Website",
+      imagesrc : "nathansnow.jpg",
+      tags: ["ux", "dev"],
+    },{
+      projectName : "LARA - Automated Administration Assistant",
+      imagesrc : "nathansnow.jpg",
+      tags: ["dev"],
+    },{
+      projectName : "AgriVita",
+      imagesrc : "nathansnow.jpg",
+      tags: ["ux", "brand"],
+    }
+  ]
+}
 
 class Header extends Component {
   render(){
@@ -30,56 +51,46 @@ class PortfolioFilter extends Component {
   }
 }
 
-
-class Portfolio extends Component {
+class PortfolioItem extends Component{
   render(){
     return(
-      <div className="portfolio">
-
-        <div className="portfolio_item">
-          <img className="portfolio_item_image" src="http://www.nsgd.com.au/nathansnow.jpg" alt=""/>
-	         <span className="portfolio_item_label">Simmo's Offroad Tours Website Design</span>
-	         <div className="portfolio_item_gradient"></div>
-        </div>
-
-        <div className="portfolio_item">
-          <img className="portfolio_item_image" src="http://www.nsgd.com.au/nathansnow.jpg" alt=""/>
-           <span className="portfolio_item_label">Simmo's Offroad Tours Website Design</span>
-           <div className="portfolio_item_gradient"></div>
-        </div>
-
-        <div className="portfolio_item">
-          <img className="portfolio_item_image" src="http://www.nsgd.com.au/nathansnow.jpg" alt=""/>
-           <span className="portfolio_item_label">Simmo's Offroad Tours Website Design</span>
-           <div className="portfolio_item_gradient"></div>
-        </div>
-
-        <div className="portfolio_item">
-          <img className="portfolio_item_image" src="http://www.nsgd.com.au/nathansnow.jpg" alt=""/>
-           <span className="portfolio_item_label">Simmo's Offroad Tours Website Design</span>
-           <div className="portfolio_item_gradient"></div>
-        </div>
-
-        <div className="portfolio_item">
-          <img className="portfolio_item_image" src="http://www.nsgd.com.au/nathansnow.jpg" alt=""/>
-           <span className="portfolio_item_label">Simmo's Offroad Tours Website Design</span>
-           <div className="portfolio_item_gradient"></div>
-        </div>
-
-        <div className="portfolio_item">
-          <img className="portfolio_item_image" src="http://www.nsgd.com.au/nathansnow.jpg" alt=""/>
-           <span className="portfolio_item_label">Simmo's Offroad Tours Website Design</span>
-           <div className="portfolio_item_gradient"></div>
-        </div>
-
-
-
-
+      <div className="portfolio_item">
+        <img className="portfolio_item_image" src={"http://www.nsgd.com.au/" + this.props.data.imagesrc} alt=""/>
+         <span className="portfolio_item_label">{this.props.data.projectName}</span>
+         <div className="portfolio_item_gradient"></div>
       </div>
     )
   }
 }
 
+
+class Portfolio extends Component {
+  constructor(props) {
+    super(props);
+    this.state = portfolioInfo;
+  }
+  render(){
+    return(
+      <div className="portfolio">
+        {this.state.projects.map(projectData =>
+          <PortfolioItem data={projectData}/>
+        )}
+      </div>
+    )
+  }
+}
+
+
+class AboutMe extends Component {
+  render(){
+    return(
+      <div className="aboutMe">
+        <h2>About Me</h2>
+        <p></p>
+      </div>
+    )
+  }
+}
 
 
 
@@ -101,6 +112,7 @@ class App extends Component {
         <PortfolioFilter/>
         <Portfolio/>
         <hr/>
+
       </div>
     );
   }

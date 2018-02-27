55beb317b70d287678399a30e36d0c13578e9dc1
Nathan <nathan@nsgd.com.au>
2018-02-28 00:14:33 +1100

    Resume & Hover logo
    - Added content to Resume (WIP)
    - Implemented SVG logo for testing (including animated hover effect)

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,31 @@
 import React from 'react';
 import logo from '../logo.svg';
-import {NavLink} from 'react-router-dom';
+import {Link, NavLink} from 'react-router-dom';
+
+class LogoReact extends React.Component {
+  render() {
+    return (
+      <svg version="1.1" className="header__logo" x="0px" y="0px" viewBox="0 0 158.6 102.8" style={{height: '60px', width: 'auto'}}>
+      <g>
+      	<path className="outline" d="M7,51.1L34.1,6h24.4l-23,38.8l0,0l-3.8,6.3L59,96.5H34.3L7,51.1z M53.1,26.8L65.5,6h0.4l39.2,69.4L92.5,96.5
+      		L53.1,26.8z M121.9,58.7l4.6-7.6L99.3,6h24.8l27.1,45.1L124,96.5H99.5L121.9,58.7z M88.8,0l5.4,9l25.3,42.1l-2.4,3.9l0,0l-0.3,0.5
+      		l-8.3,13.8L71,3l-1.7-3H30.7L29,2.9L1.8,48L0,51.1l1.8,3.1l27.3,45.4l1.7,2.9h3.4H59h10.5l-5.4-9L38.7,51.1l11-18.3l37.6,66.6
+      		l1.7,3h3.5H124h3.4l1.7-2.9l27.3-45.4l1.8-3.1l-1.8-3.1l-27.2-45L127.5,0H88.8z"/>
+      </g>
+      <g>
+      	<polygon className="solid" points="59.1,96.5 34.3,96.5 7,51.1 34.1,6 58.5,6 35.5,44.8 35.6,44.8 31.8,51.1 	"/>
+      	<polygon className="solid" points="53.1,26.8 65.5,6 65.8,6 105.1,75.4 92.5,96.5 	"/>
+      	<polygon className="solid" points="124,96.5 99.5,96.5 121.9,58.7 126.5,51.1 99.3,6 124.1,6 151.3,51.1 	"/>
+      </g>
+      </svg>
+    );
+  }
+}
+
 
 const Header = () => (
   <header className="header" style={{padding: '20px'}}>
-    <img src={logo} style={{height:'60px'}} alt="Nathan Simpson" />
+    <Link exact to="/"><LogoReact/></Link>
     <nav className="nav">
       <ul>
         <li><NavLink exact to="/" className="nav__item" activeClassName="nav__item--active">Home</NavLink></li>
--- src/components/Resume.js
+++ src/components/Resume.js
@@ -1,11 +1,150 @@
 import React from 'react';
 
+class Experience extends React.Component {
+  render() {
+    return(
+      <section>
+        <h2>Experience</h2>
+
+        <div className="resume__item">
+          <h3>eLearning Development, Training & Support</h3>
+          <p>ITC Learning - Crows Nest NSW</p>
+          <p>2016 - Present</p>
+          <ul>
+            <li>Working with project managers, clients and other designers throughout design projects</li>
+            <li>Training clients in the use of specialised software</li>
+            <li>Providing technical support for clients via phone and email</li>
+            <li>Design, implementation and maintenance of a new website for the company</li>
+          </ul>
+        </div>
+
+        <div className="resume__item">
+          <h3>Freelance Graphic + Web Designer</h3>
+          <p>2013 - Present</p>
+          <ul>
+            <li>Offering Logo Design, Web Design, Brochures and Posters</li>
+            <li>Client relations skills</li>
+            <li>Project Management</li>
+            <li>Understanding of Client’s needs</li>
+          </ul>
+        </div>
+
+        <div className="resume__item">
+          <h3>Sales, Support & Training</h3>
+          <p>Leading Edge Computers - Orange NSW</p>
+          <p>2013 - 2015</p>
+          <ul>
+            <li>Customer relations skills</li>
+            <li>Preparing marketing collateral across many mediums</li>
+            <li>Assisted Technical team with duties</li>
+          </ul>
+        </div>
+      </section>
+    );
+  }
+}
+
+class Education extends React.Component {
+  render() {
+    return(
+      <section>
+        <h2>Education</h2>
+
+        <div className="resume__item">
+          <h3>Bachelor of Design (Visual Communication)</h3>
+          <p>Western Sydney University - Parramatta NSW</p>
+          <p>2015 - 2018</p>
+          <ul>
+            <li>Working with project managers, clients and other designers throughout design projects</li>
+            <li>Training clients in the use of specialised software</li>
+            <li>Providing technical support for clients via phone and email</li>
+            <li>Design, implementation and maintenance of a new website for the company</li>
+          </ul>
+        </div>
+
+        <div className="resume__item">
+          <h3>High School Certificate</h3>
+          <p>Orange Christian School - Orange NSW</p>
+          <p>2012 - 2013</p>
+        </div>
+      </section>
+    );
+  }
+}
+
+
+class Interests extends React.Component {
+  render() {
+    return(
+      <section className="resume__item">
+        <h2>Interests</h2>
+        <ul>
+          <li>Web & App Design</li>
+          <li>Computers & Technology</li>
+          <li>Flying Drones</li>
+          <li>Skiing</li>
+          <li>Watching Motorsport</li>
+        </ul>
+      </section>
+    );
+  }
+}
+
+
+class Skills extends React.Component {
+  render() {
+    return(
+      <section className="resume__item">
+        <h2>Skills</h2>
+        <ul>
+          <li>Adobe Photoshop & Illustrator</li>
+          <li>Adobe XD</li>
+          <li>Adobe Animate & After Effects</li>
+          <li>HTML, CSS, JavaScript</li>
+          <li>Swift</li>
+          <li>macOS & Windows</li>
+          <li>Microsoft Office</li>
+        </ul>
+      </section>
+    );
+  }
+}
+
+class Volunteer extends React.Component {
+  render() {
+    return(
+      <section className="resume__item">
+        <h2>Skills</h2>
+        <h3>Service Host and Kids Church</h3>
+        Elevation Church Penrith
+        2016 - Present
+        <h3>Volunteer</h3>
+        NSW State Emergency Service Orange
+        2014 - 2015
+      </section>
+    );
+  }
+}
+
+
+
 export default class Resume extends React.Component {
   render() {
     return(
       <div>
-        <h2>Resume</h2>
-        <p>Resume Content</p>
+        <h1>Resume</h1>
+
+        <h2>Career Objectives</h2>
+        <p>Bachelor of Design graduate seeking an entry level role in Web and Application Design, with the opportunity to further my interests in development. I can offer strong visual design and computer skills with a sound knowledge in HTML, CSS, JavaScript and Swift.</p>
+        <div style={{width:'60%'}}>
+          <Experience/>
+          <Education/>
+        </div>
+        <div style={{width:'40%'}}>
+          <Interests/>
+          <Skills/>
+          <Volunteer/>
+        </div>
       </div>
     );
   }

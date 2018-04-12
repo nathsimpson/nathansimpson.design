e8e538a035692ee2428572f42be7b1c0cfa1653f
Nathan Simpson <hello@nathansimpson.design>
2018-04-12 23:32:24 +1000

    Removed my ugly mug from the top

--- src/components/Home.js
+++ src/components/Home.js
@@ -9,13 +9,9 @@ class Home extends React.Component {
  render(){
    return(
      <main>
-       <div className="row" id="intro">
-         <div className="hero-half">
-         </div>
-         <div className="hero-half">
-           <h1 style={{fontSize: '3em', fontWeight:500, lineHeight:'1em', margin: '10px 0px',}}><span style={{color:'white'}}>Hi I&apos;m</span> <br/>Nathan Simpson</h1>
+       <div id="intro" style={{textAlign: 'center', margin: '80px'}}>
+           <h1 style={{fontSize: '3em', fontWeight:500, lineHeight:'1em', margin: '10px 0px',}}><span style={{color:'white'}}>Hi I&apos;m</span> Nathan Simpson</h1>
            <span>Designer and Frontend Developer from Sydney.</span>
-         </div>
        </div>
        <hr/>
        <Portfolio projects={this.props.projects} limit={7}/>

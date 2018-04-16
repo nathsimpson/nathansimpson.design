0601a12329ad0824e13fce0e2c2aea8011f22c1c
Nathan Simpson <hello@nathansimpson.design>
2018-04-16 20:13:23 +1000

    Added Dribbble to Footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,7 +2,7 @@ import React from 'react'
 import fontawesome from '@fortawesome/fontawesome'
 import FontAwesomeIcon from '@fortawesome/react-fontawesome'
 import { faHeart } from '@fortawesome/fontawesome-free-regular'
-import { faTwitter, faInstagram, faCodepen, faLinkedin, faGithub, faReact, faYoutube } from '@fortawesome/fontawesome-free-brands'
+import { faTwitter, faInstagram, faCodepen, faLinkedin, faGithub, faReact, faDribbble, faYoutube } from '@fortawesome/fontawesome-free-brands'
 import logo from '../logo.svg';
 
 const Footer = () => (
@@ -14,6 +14,7 @@ const Footer = () => (
       <li><a href="https://www.linkedin.com/in/nathan-simpson-71512a75?trk" target="_blank"><FontAwesomeIcon icon={faLinkedin} /></a></li>
       <li><a href="https://github.com/nathansimpson95" target="_blank"><FontAwesomeIcon icon={faGithub} /></a></li>
       <li><a href="https://codepen.io/nathansimpson/" target="_blank"><FontAwesomeIcon icon={faCodepen} /></a></li>
+      <li><a href="https://dribbble.com/nathansimpson" target="_blank"><FontAwesomeIcon icon={faDribbble} /></a></li>
       <li><a href="https://www.youtube.com/channel/UCa__hNMzVWIQOHErctX0leg" target="_blank"><FontAwesomeIcon icon={faYoutube} /></a></li>
     </ul>
     <FontAwesomeIcon icon="Heart"/>

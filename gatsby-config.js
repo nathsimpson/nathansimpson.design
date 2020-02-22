c3019f4d2b8e82ef29bccbec68885a57c30dcd09
Nathan Simpson <hello@nathansimpson.design>
2020-02-23 00:57:23 +1100

    configure manifest

--- gatsby-config.js
+++ gatsby-config.js
@@ -33,20 +33,19 @@ module.exports = {
         name: 'projects'
       }
     },
-    'gatsby-transformer-remark'
-
-    // {
-    //   resolve: `gatsby-plugin-manifest`,
-    //   options: {
-    //     name: `gatsby-starter-default`,
-    //     short_name: `starter`,
-    //     start_url: `/`,
-    //     background_color: `#663399`,
-    //     theme_color: `#663399`,
-    //     display: `minimal-ui`,
-    //     icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
-    //   },
-    // },
+    'gatsby-transformer-remark',
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: `Nathan Simpson. Designer + Developer`,
+        short_name: `Nathan S.`,
+        start_url: `/`,
+        background_color: `#fa6d01`,
+        theme_color: `#fa6d01`,
+        display: `minimal-ui`,
+        icon: `src/images/favicon-32x32.png` // This path is relative to the root of the site.
+      }
+    }
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.app/offline
     // 'gatsby-plugin-offline',

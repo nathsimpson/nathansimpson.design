5d268ce3471da560ad10908f6a3fca6c0d569cb2
Nathan Simpson <hello@nathansimpson.design>
2020-08-23 16:12:54 +1000

    WIP

---
+++ functions/getDribbbleShots.js
@@ -0,0 +1,22 @@
+import fetch from 'node-fetch';
+
+const ACCESS_TOKEN = process.env.DRIBBBLE_ACCESS_TOKEN;
+const API_ENDPOINT = `https://api.dribbble.com/v2/user/shots?access_token=${ACCESS_TOKEN}`;
+
+exports.handler = async (event, context) => {
+  return fetch(API_ENDPOINT, { headers: { Accept: 'application/json' } })
+    .then(response => response.json())
+    .then(data =>
+      data.map(shot => ({
+        description: shot.description.replace(/(<([^>]+)>)/gi, ''),
+        image: shot.images.normal,
+        link: shot.html_url
+      }))
+    )
+    .then(data => ({
+      statusCode: 200,
+      body: data
+    }))
+
+    .catch(error => ({ statusCode: 422, body: String(error) }));
+};

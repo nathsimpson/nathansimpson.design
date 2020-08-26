9c89682f690a879ba80ecf6cecffb4a932396b5c
Nathan Simpson <hello@nathansimpson.design>
2020-08-26 16:43:45 +1000

    Create lambda function for Dribbble shots (#14)

---
+++ functions/getDribbbleShots.js
@@ -0,0 +1,21 @@
+const fetch = require('node-fetch').default;
+
+const ACCESS_TOKEN = process.env.DRIBBBLE_ACCESS_TOKEN;
+const API_ENDPOINT = `https://api.dribbble.com/v2/user/shots?access_token=${ACCESS_TOKEN}`;
+
+exports.handler = async (event, context) => {
+  return fetch(API_ENDPOINT, { headers: { Accept: 'application/json' } })
+    .then(response => response.json())
+    .then(data =>
+      data.map(shot => ({
+        description: shot.description.replace(/(<([^>]+)>)/gi, ''),
+        image: shot.images.normal,
+        link: shot.html_url
+      }))
+    )
+    .then(data => ({
+      statusCode: 200,
+      body: JSON.stringify(data)
+    }))
+    .catch(error => ({ statusCode: 422, body: JSON.stringify(error) }));
+};

5235d3889edb178f9e7d67ccf60b91a29eaa69cd
Nathan Simpson <hello@nathansimpson.design>
2020-11-09 23:53:01 +1100

    Link to About and Blog from home

--- src/pages/index.js
+++ src/pages/index.js
@@ -20,14 +20,22 @@ const App = ({ data }) => (
       }}
     >
       <Logo />
-      <h1 css={{ margin: 0 }}>Nathan Simpson</h1>
-
-      <p css={{ margin: 0 }}>
-        UI Designer &amp; Developer at{' '}
+      <h1>Nathan Simpson</h1>
+      <p>
+        I'm a UI Designer &amp; Developer at{' '}
         <a href="https://thinkmill.com.au">Thinkmill</a> in Sydney, Australia.
         I'm passionate about Design Systems, and building products from concept
         to delivery.
       </p>
+
+      <div css={{ marginBottom: 12 }}>
+        <a css={{ display: 'inline', marginRight: 16 }} href="/about">
+          About
+        </a>
+        <a css={{ display: 'inline', marginRight: 16 }} href="/blog">
+          Blog
+        </a>
+      </div>
     </div>
 
     <Portfolio data={data} />
@@ -39,35 +47,6 @@ const App = ({ data }) => (
     <Block>
       <h2>Talks</h2>
       <Talks data={data} />
-    </Block>
-
-    <Block>
-      <h2>Bio</h2>
-
-      <p>
-        Nathan Simpson is a frontend developer and user-interface designer based
-        in Sydney, Australia. He is experienced with building and using Design
-        Systems, and is passionate about building useful applications from
-        concept to delivery.
-      </p>
-
-      <p>
-        Born in Orange NSW, Nathan started designing logos and posters for
-        family members from 13 year old. He then started developing a taste for
-        digital experiences, and began designing websites and building them in
-        HTML and CSS. After "accidentally" learning some PHP through creating
-        custom Wordpress themes for clients, Nathan started getting into
-        JavaScript and React.
-      </p>
-
-      <p>
-        Nathan has a Bachelor of Design (Visual Communication) from Western
-        Sydney University, where he studied Web and Interactive Design, Data
-        Visualisation, and Programming Fundamentals. He also took an elective at
-        the Sydney School of Entrepreneurship. Nathan learnt Frontend
-        Development technologies like Javascript and React through Treehouse.
-        Nathan is a regular attendee at SydJS and React Sydney meetups.
-      </p>
     </Block>
 
     <Contact />

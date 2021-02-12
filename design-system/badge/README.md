b21037ea6445674316c36995f643fed928c78d62
Nathan Simpson <hello@nathansimpson.design>
2021-02-13 02:21:11 +1100

    Badge props

--- design-system/badge/README.md
+++ design-system/badge/README.md
@@ -6,8 +6,29 @@ type: design-system
 
 # Badge
 
-This is a badge component.
+This is a badge component, also known as a Tag.
+
+A badge will always capitalise the first letter in the label.
 
 ```jsx
-<Badge label="Badge" />
+<Badge label="Badge" size="medium" />
+```
+
+There are two sizes for Badge.
+
+- small
+- medium
+
+There are three colours
+
+- brand
+- decorativeGreen
+- decorativePurple
+
+```jsx
+<>
+  <Badge label="brand" color="brand" />
+  <Badge label="decorativeGreen" color="decorativeGreen" />
+  <Badge label="decorativePurple" color="decorativePurple" />
+</>
 ```
--- design-system/badge/index.tsx
+++ design-system/badge/index.tsx
@@ -1,6 +1,6 @@
 /** @jsx jsx */
 import { jsx } from '@emotion/core';
-import { palette, borderRadius } from '../theme';
+import { colors, radii } from '../theme';
 import hexAlpha from 'hex-alpha';
 
 const sizes = {
@@ -8,28 +8,33 @@ const sizes = {
   small: 3
 };
 
+type BadgeProps = {
+  label: string;
+  // as: any;
+  color: 'brand' | 'decorativeGreen' | 'decorativePurple';
+  size: keyof typeof sizes;
+};
+
 export const Badge = ({
   label,
   as,
-  color = 'orange',
-  size = 'medium',
-  style
-}) => {
+  color = 'brand',
+  size = 'medium'
+}: BadgeProps) => {
   const Tag = as || 'div';
   const formattedLabel = label.charAt(0).toUpperCase() + label.slice(1);
   return (
     <Tag
       css={{
-        border: `1px solid ${palette[color]}`,
-        color: palette[color],
-        backgroundColor: hexAlpha(palette[color], 0.2),
-        borderRadius: borderRadius.sm,
+        border: `1px solid ${colors[color]}`,
+        color: colors[color],
+        backgroundColor: hexAlpha(colors[color], 0.2),
+        borderRadius: radii.sm,
         fontSize: sizes[size] * 4,
         paddingTop: sizes[size],
         paddingBottom: sizes[size],
         paddingLeft: sizes[size] * 2,
-        paddingRight: sizes[size] * 2,
-        ...style
+        paddingRight: sizes[size] * 2
       }}
     >
       {formattedLabel}

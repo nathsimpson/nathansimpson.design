cc0f7049cdb351c855db260df7f4cbdf55d040ec
Nathan Simpson <hello@nathansimpson.design>
2021-05-09 11:59:51 +1000

    update contact form

--- src/components/contactform.js
+++ src/components/contactform.js
@@ -1,12 +1,11 @@
 /** @jsx jsx */
 import { jsx } from '@emotion/core';
-import styled from '@emotion/styled';
-import { radii, colors, fontsizes } from '../../design-system/theme';
-import hexAlpha from 'hex-alpha';
+import { Button } from '../../design-system/button';
+
+import { TextInput, TextArea } from '../../design-system/textinput';
 
 export const ContactForm = () => (
   <div style={{ width: '100%' }}>
-    <h2 style={{ textAlign: 'center' }}>Contact Me</h2>
     <form
       style={{ maxWidth: '400px', margin: '40px auto' }}
       name="contact"
@@ -15,98 +14,12 @@ export const ContactForm = () => (
       netlify="true"
       data-netlify-honeypot="bot-field"
     >
-      <Input type="text" name="name" label="Name" isRequired />
-      <Input type="email" name="email" label="Email" isRequired />
-      <Input type="tel" name="phone" label="Phone" />
-      <Select name="idliketo" label="I would like to...">
-        <option>Chat with you</option>
-        <option>Contract you</option>
-      </Select>
-      <TextInput name="message" label="Message" isRequired />
+      <TextInput type="text" name="name" label="Name" isRequired />
+      <TextInput type="email" name="email" label="Email" isRequired />
+      <TextInput type="tel" name="phone" label="Phone" />
+      <TextArea name="message" label="Message" isRequired />
       <input type="hidden" name="form-name" value="contact" />
-      <FormSubmit type="submit" />
+      <Button type="submit" label="Submit" weight="primary" />
     </form>
   </div>
-);
-
-const baseInputStyles = {
-  background: colors.input.background,
-  color: colors.input.foreground,
-  outlineColor: colors.brand,
-  border: `1px solid ${colors.border}`,
-  fontSize: '1em',
-  display: 'block',
-  width: '100%',
-  maxWidth: 400,
-  padding: '0.75rem',
-  borderRadius: radii.medium,
-  boxSizing: 'border-box',
-  transition: 'all 0.5s',
-  '&:hover': {
-    background: colors.input.hover.background,
-    color: colors.input.hover.foreground
-  },
-  '&:active, &:focus': {
-    background: colors.input.active.background,
-    color: colors.input.active.foreground
-  }
-};
-
-const Input = props => (
-  <div css={{ marginBottom: 12 }}>
-    <Label label={props.label} isRequired={props.isRequired} />
-    <input css={baseInputStyles} {...props} />
-  </div>
-);
-
-const Select = props => (
-  <div css={{ marginBottom: 12 }}>
-    <Label label={props.label} isRequired={props.isRequired} />
-    <select
-      css={{
-        ...baseInputStyles,
-        height: 50
-      }}
-      {...props}
-    />
-  </div>
-);
-
-const TextInput = props => (
-  <div css={{ marginBottom: 12 }}>
-    <Label label={props.label} isRequired={props.isRequired} />
-    <textArea css={baseInputStyles} {...props} />
-  </div>
-);
-
-const FormSubmit = styled.input({
-  background: hexAlpha(colors.link, 0.2),
-  border: 'none',
-  fontSize: '1em',
-  color: colors.link,
-  marginTop: 6,
-  padding: '12px 24px',
-  borderRadius: radii.medium,
-  boxSizing: 'border-box',
-  transition: 'all 0.5s',
-  fontWeight: 700,
-
-  '&:hover': {
-    background: colors.link,
-    color: colors.foregroundEmphasis
-  }
-});
-
-const Label = ({ label, isRequired }) => (
-  <label
-    css={{
-      fontSize: fontsizes.medium,
-      color: colors.foreground,
-      display: 'block',
-      marginBottom: 6
-    }}
-  >
-    {label}
-    {isRequired && <span css={{ color: colors.orange, marginLeft: 2 }}>*</span>}
-  </label>
 );

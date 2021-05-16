79f79c599df746a89e03a4badd20923b35f399b1
Nathan Simpson <hello@nathansimpson.design>
2021-05-16 15:09:38 +1000

    replace tab things

--- src/templates/sw.template.js
+++ src/templates/sw.template.js
@@ -1,60 +1,60 @@
 self
-	.oninstall = event => {
-		event
-			.waitUntil( caches
-				.open( {{NAME}} )
-				.then( cache => {
-					return cache.addAll([ {{FILES}} ]);
-			})
-		);
+  .oninstall = event => {
+    event
+      .waitUntil( caches
+        .open( {{NAME}} )
+        .then( cache => {
+          return cache.addAll([ {{FILES}} ]);
+      })
+    );
 
-	// replaces the old SW automatically
-	self.skipWaiting();
+  // replaces the old SW automatically
+  self.skipWaiting();
 };
 
 self
-	.onactivate = event => {
-		event
-			.waitUntil( caches
-				.keys()
-				.then( cacheNames => {
-					const deleteOldCaches = cacheNames.map( cacheName => {
-						if ( cacheName !== {{NAME}} ) {
-							return caches.delete( cacheName );
-						}
-						// Leave the current cache alone
-					});
+  .onactivate = event => {
+    event
+      .waitUntil( caches
+        .keys()
+        .then( cacheNames => {
+          const deleteOldCaches = cacheNames.map( cacheName => {
+            if ( cacheName !== {{NAME}} ) {
+              return caches.delete( cacheName );
+            }
+            // Leave the current cache alone
+          });
 
-					//removing old cache
-					return Promise.all( deleteOldCaches );
-				}
-			)
-		);
+          //removing old cache
+          return Promise.all( deleteOldCaches );
+        }
+      )
+    );
 
-		//ensures all open tabs use new service worker
-		self.clients.claim();
+    //ensures all open tabs use new service worker
+    self.clients.claim();
 };
 
 self
-	.addEventListener('fetch', event => {
-		if( event.request.method !== 'POST' ) {
-			event.respondWith(
-				caches
-					.open( {{NAME}} )
-					.then( cache => cache
-						.match( event.request ) // checks if it's in cache
-						.then( cacheResponse => {
-							// fetches latest resource from server
-							const fetchPromise = fetch( event.request )
-								.then( networkResponse => {
-									cache.put(event.request, networkResponse.clone());
-									return networkResponse;
-							});
+  .addEventListener('fetch', event => {
+    if( event.request.method !== 'POST' ) {
+      event.respondWith(
+        caches
+          .open( {{NAME}} )
+          .then( cache => cache
+            .match( event.request ) // checks if it's in cache
+            .then( cacheResponse => {
+              // fetches latest resource from server
+              const fetchPromise = fetch( event.request )
+                .then( networkResponse => {
+                  cache.put(event.request, networkResponse.clone());
+                  return networkResponse;
+              });
 
-							return cacheResponse || fetchPromise;
-					})
-			)
-			.catch( error => console.error( error ) )
-		);
-	}
+              return cacheResponse || fetchPromise;
+          })
+      )
+      .catch( error => console.error( error ) )
+    );
+  }
 });

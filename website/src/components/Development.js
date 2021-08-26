cbdf1c107758de81ecb66f0d15f430a460aae8f9
Nathan Simpson <hello@nathansimpson.design>
2021-08-27 00:35:37 +1000

    Remove unused var

--- website/src/components/Development.js
+++ website/src/components/Development.js
@@ -2,7 +2,6 @@
 import { jsx } from '@emotion/core';
 import { Tag } from '@design-system/tag';
 import { Cluster } from '@design-system/cluster';
-import { Stack } from '@design-system/stack';
 
 import { Heading, Text } from '@design-system/typography';
 import { TextLink } from '@design-system/textlink';

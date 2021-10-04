b23b164249dd933a0dbfd827eee38c53073501c0
Nathan Simpson <hello@nathansimpson.design>
2021-10-04 16:35:17 +1100

    missed one

--- design-system/cluster/cluster.tsx
+++ design-system/cluster/cluster.tsx
@@ -1,5 +1,5 @@
 /** @jsx jsx */
-import { jsx } from '@emotion/core';
+import { jsx } from '@emotion/react';
 import { Children, ReactNode } from 'react';
 import { Box, BoxProps } from '../box';
 import { spacing, SpacingType } from '@nathsimpson/theme';

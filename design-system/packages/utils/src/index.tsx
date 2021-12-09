f957585df6d45a04336453784d0d431c84b99a06
Nathan Simpson <hello@nathansimpson.design>
2021-12-09 18:49:12 +1100

    button tints. active colors

--- design-system/packages/utils/src/index.tsx
+++ design-system/packages/utils/src/index.tsx
@@ -1,5 +1,8 @@
 export { default as hexAlpha } from 'hex-alpha';
+export { getContrastColor, getContrastRatio, getWcagScore } from 'hex-a11y';
+
 export { forwardRefWithAs } from './forwardRefWithAs';
 export { useMediaQuery } from './useMediaQuery';
+export { getTintColor } from './getTintColor';
 
 export type ResponsiveProp<T> = T | readonly (T | null)[];

d7ee86de7ebe02a201647e3de2e7fb8b2fff1887
Nathan Simpson <hello@nathansimpson.design>
2022-07-23 18:34:07 +1000

    Refactor of box styles

--- design-system/packages/box/src/styles.tsx
+++ design-system/packages/box/src/styles.tsx
@@ -5,68 +5,23 @@ import {
   SpacingType,
   RadiiType
 } from '@nathsimpson/theme';
-import { useMediaQuery, ResponsiveProp } from '@nathsimpson/utils';
-import { ElementType, ReactNode } from 'react';
+import {
+  mapResponsiveProp,
+  useMediaQuery,
+  ResponsiveProp
+} from '@nathsimpson/utils';
 
-export type BoxMarginProps = {
-  /** Margin */
+type BoxMarginProps = {
   margin?: ResponsiveProp<SpacingType>;
-  /** Margin bottom */
   marginBottom?: ResponsiveProp<SpacingType>;
-  /** Margin left */
   marginLeft?: ResponsiveProp<SpacingType>;
-  /** Margin right */
   marginRight?: ResponsiveProp<SpacingType>;
-  /** Margin top */
   marginTop?: ResponsiveProp<SpacingType>;
-  /** Margin horizontal */
   marginX?: ResponsiveProp<SpacingType>;
-  /** Margin vertical */
   marginY?: ResponsiveProp<SpacingType>;
 };
 
-export type BoxPaddingProps = {
-  /** Padding */
-  padding?: ResponsiveProp<SpacingType>;
-  /** Padding bottom */
-  paddingBottom?: ResponsiveProp<SpacingType>;
-  /** Padding left */
-  paddingLeft?: ResponsiveProp<SpacingType>;
-  /** Padding right */
-  paddingRight?: ResponsiveProp<SpacingType>;
-  /** Padding top */
-  paddingTop?: ResponsiveProp<SpacingType>;
-  /** Padding horizontal */
-  paddingX?: ResponsiveProp<SpacingType>;
-  /** Padding vertical */
-  paddingY?: ResponsiveProp<SpacingType>;
-};
-
-export type BoxRadiiProps = {
-  /** Radius */
-  rounding?: RadiiType;
-  /** Radius bottom */
-  roundingBottom?: RadiiType;
-  /** Radius left */
-  roundingLeft?: RadiiType;
-  /** Radius right */
-  roundingRight?: RadiiType;
-  /** Radius top */
-  roundingTop?: RadiiType;
-};
-
-export type FlexProps = {
-  /** Define the initial main size of a flex item. */
-  basis?: number | string;
-  /** You can pass shorthand values directly to the CSS flex property. e.g.<Box flex="0 1 auto" /> */
-  flex?: number | string;
-  /** Define grow "factor" of a flex item. It accepts a unitless value, which dictates the amount of available space inside the flex container the item should take up. */
-  grow?: number;
-  /** Define shrink "factor" of a flex item. It accepts a unitless value, which dictates the amount of available space inside the flex container the item should take up. */
-  shrink?: number;
-};
-
-export const getMarginStyles = ({
+const getMarginStyles = ({
   margin,
   marginBottom,
   marginLeft,
@@ -75,8 +30,6 @@ export const getMarginStyles = ({
   marginX,
   marginY
 }: BoxMarginProps) => {
-  const { mapResponsiveProp } = useMediaQuery();
-
   const mb = marginBottom || marginY || margin;
   const mt = marginTop || marginY || margin;
   const ml = marginLeft || marginX || margin;
@@ -90,7 +43,17 @@ export const getMarginStyles = ({
   };
 };
 
-export const getPaddingStyles = ({
+type BoxPaddingProps = {
+  padding?: ResponsiveProp<SpacingType>;
+  paddingBottom?: ResponsiveProp<SpacingType>;
+  paddingLeft?: ResponsiveProp<SpacingType>;
+  paddingRight?: ResponsiveProp<SpacingType>;
+  paddingTop?: ResponsiveProp<SpacingType>;
+  paddingX?: ResponsiveProp<SpacingType>;
+  paddingY?: ResponsiveProp<SpacingType>;
+};
+
+const getPaddingStyles = ({
   padding,
   paddingBottom,
   paddingLeft,
@@ -99,8 +62,6 @@ export const getPaddingStyles = ({
   paddingX,
   paddingY
 }: BoxPaddingProps) => {
-  const { mapResponsiveProp } = useMediaQuery();
-
   const pb = paddingBottom || paddingY || padding;
   const pt = paddingTop || paddingY || padding;
   const pl = paddingLeft || paddingX || padding;
@@ -114,15 +75,26 @@ export const getPaddingStyles = ({
   };
 };
 
-export const getRadiiStyles = ({
+type BoxRadiiProps = {
+  /** Radius */
+  rounding?: RadiiType;
+  /** Radius bottom */
+  roundingBottom?: RadiiType;
+  /** Radius left */
+  roundingLeft?: RadiiType;
+  /** Radius right */
+  roundingRight?: RadiiType;
+  /** Radius top */
+  roundingTop?: RadiiType;
+};
+
+const getRadiiStyles = ({
   rounding,
   roundingBottom,
   roundingLeft,
   roundingRight,
   roundingTop
 }: BoxRadiiProps) => {
-  const { mapResponsiveProp } = useMediaQuery();
-
   const borderBottomLeftRadius = roundingBottom || roundingLeft || rounding;
   const borderBottomRightRadius = roundingBottom || roundingRight || rounding;
   const borderTopLeftRadius = roundingTop || roundingLeft || rounding;
@@ -142,68 +114,88 @@ export const getRadiiStyles = ({
   };
 };
 
-export const getFlexStyles = ({ flex, basis, grow, shrink }: FlexProps) => {
-  if (flex) {
-    return {
-      flex: flex
-    };
-  } else {
-    return {
-      flexBasis: basis,
-      flexGrow: grow,
-      flexShrink: shrink
-    };
-  }
+type BoxLayoutProps = {
+  display?: 'block' | 'inline-block' | 'flex' | 'inline-flex';
+  height?: ResponsiveProp<number | string>;
+  width?: ResponsiveProp<number | string>;
+  gap?: ResponsiveProp<SpacingType>;
+  /** Defines the alignment of items along the cross-axis of the flex container. */
+  alignItems?: 'center' | 'flex-end' | 'flex-start' | 'stretch';
+  justifyContent?:
+    | 'space-around'
+    | 'space-between'
+    | 'center'
+    | 'flex-end'
+    | 'space-evenly'
+    | 'flex-start'
+    | 'stretch';
+  /** Define the initial main size of a flex item. */
+  flexBasis?: number | string;
+  /** Define grow "factor" of a flex item. It accepts a unitless value, which dictates the amount of available space inside the flex container the item should take up. */
+  flexGrow?: number;
+  /** Define shrink "factor" of a flex item. It accepts a unitless value, which dictates the amount of available space inside the flex container the item should take up. */
+  flexShrink?: number;
+  /** Defines the distribution of space between items along the main-axis of the flex container. */
+  flexDirection?: 'row' | 'row-reverse' | 'column' | 'column-reverse';
 };
 
-export const orientationMap = {
-  horizontal: 'row',
-  vertical: 'column'
-} as const;
+const layoutStyles = ({
+  display,
+  height,
+  gap,
+  width,
+  flexBasis,
+  flexGrow,
+  flexShrink,
+  flexDirection
+}: BoxLayoutProps) => ({
+  display,
+  height: height,
+  width,
+  gap,
+  flexBasis,
+  flexGrow,
+  flexShrink,
+  flexDirection
+});
 
-export const alignmentMap = {
-  center: 'center',
-  end: 'flex-end',
-  start: 'flex-start',
-  stretch: 'stretch'
-} as const;
-
-export const justificationMap = {
-  around: 'space-around',
-  between: 'space-between',
-  center: 'center',
-  end: 'flex-end',
-  even: 'space-evenly',
-  start: 'flex-start',
-  stretch: 'stretch'
-} as const;
-
-export const useBoxColors = () => {
+type BoxColorProps = {
+  /** Background color */
+  bg?: 'none' | 'base' | 'emphasis';
+};
+const colorStyles = ({ bg }: BoxColorProps) => {
   const { colors } = useTheme();
-  return {
+  const bgColors = {
     none: 'none',
     base: colors.background.default,
     emphasis: colors.background.emphasis
-  } as const;
+  };
+  return {
+    background: bg && mapResponsiveProp(bg, bgColors)
+  };
 };
 
 export type BoxProps = BoxMarginProps &
   BoxPaddingProps &
-  BoxRadiiProps & {
-    as?: ElementType;
-    /** Background color */
-    bg?: 'none' | 'base' | 'emphasis';
-    /** The content of this container. */
-    children?: ReactNode | ReactNode[] | string;
-    /** Height */
-    height?: number;
-    /** Width */
-    width?: number;
-  };
+  BoxColorProps &
+  BoxRadiiProps &
+  BoxLayoutProps;
 
+// {
+//   as?: ElementType;
+//   /** The content of this container. */
+//   children?: ReactNode;
+// };
 export const boxStyles = ({
-  bg = 'none',
+  display,
   height,
+  width,
+  gap,
+  flexBasis,
+  flexGrow,
+  flexShrink,
+  flexDirection,
+  bg,
   margin,
   marginBottom,
   marginLeft,
@@ -223,45 +215,50 @@ export const boxStyles = ({
   roundingLeft,
   roundingRight,
   roundingTop,
-  width,
   ...attrs
 }: BoxProps) => {
-  const { mq, mapResponsiveProp } = useMediaQuery();
-  const marginStyles = getMarginStyles({
-    margin,
-    marginBottom,
-    marginLeft,
-    marginRight,
-    marginTop,
-    marginX,
-    marginY
-  });
-  const paddingStyles = getPaddingStyles({
-    padding,
-    paddingBottom,
-    paddingLeft,
-    paddingRight,
-    paddingTop,
-    paddingX,
-    paddingY
-  });
-  const radiiStyles = getRadiiStyles({
-    rounding,
-    roundingBottom,
-    roundingLeft,
-    roundingRight,
-    roundingTop
-  });
-  const colors = useBoxColors();
+  const { mq } = useMediaQuery();
 
   return [
     mq({
-      background: mapResponsiveProp(bg, colors),
-      height,
-      margin: marginStyles,
-      padding: paddingStyles,
-      radii: radiiStyles,
-      width
+      ...layoutStyles({
+        display,
+        height,
+        gap,
+        width,
+        flexBasis,
+        flexGrow,
+        flexShrink,
+        flexDirection
+      }),
+      ...colorStyles({
+        bg
+      }),
+      ...getMarginStyles({
+        margin,
+        marginBottom,
+        marginLeft,
+        marginRight,
+        marginTop,
+        marginX,
+        marginY
+      }),
+      ...getPaddingStyles({
+        padding,
+        paddingBottom,
+        paddingLeft,
+        paddingRight,
+        paddingTop,
+        paddingX,
+        paddingY
+      }),
+      ...getRadiiStyles({
+        rounding,
+        roundingBottom,
+        roundingLeft,
+        roundingRight,
+        roundingTop
+      })
     }),
     attrs
   ];

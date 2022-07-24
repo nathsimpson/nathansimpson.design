bc3e4adf5459ee92832e9e72200f6757d6c726f2
Nathan Simpson <hello@nathansimpson.design>
2022-07-24 12:24:44 +1000

    Move Hero to own file

---
+++ website/src/components/Hero.tsx
@@ -0,0 +1,59 @@
+/** @jsx jsx */
+import { jsx } from '@emotion/core';
+import { Stack } from '@nathsimpson/box';
+import { TextLink } from '@nathsimpson/text-link';
+import { useTheme } from '@nathsimpson/theme';
+import { Heading, Text } from '@nathsimpson/typography';
+
+export const Hero = () => {
+  const { maxWidth } = useTheme();
+
+  return (
+    <div
+      css={{
+        width: '100%'
+      }}
+    >
+      <div
+        css={{
+          maxWidth: maxWidth.page,
+          padding: '128px 24px',
+          margin: '0 auto',
+          boxSizing: 'border-box',
+          display: 'flex',
+          alignItems: 'start',
+          justifyContent: 'center',
+          flexDirection: 'column',
+          textAlign: 'left'
+        }}
+      >
+        <Stack gap="medium" css={{ maxWidth: maxWidth.content }}>
+          <Avatar />
+          <Heading level={1}>G'day, I'm Nath 👋</Heading>
+          <Text as="p" size="large">
+            I'm a UI Designer &amp; Developer at{' '}
+            <TextLink href="https://thinkmill.com.au">Thinkmill</TextLink> in
+            Sydney, Australia. I'm passionate about Design Systems, and building
+            products from concept to delivery.
+          </Text>
+        </Stack>
+      </div>
+    </div>
+  );
+};
+
+const Avatar = () => {
+  const { colors } = useTheme();
+  return (
+    <img
+      src="https://files.nathansimpson.design/avatar.jpeg"
+      alt="Nathan is smiling while sitting in a Ford Mustang."
+      css={{
+        width: 100,
+        height: 100,
+        borderRadius: 50,
+        backgroundColor: colors.text.default
+      }}
+    />
+  );
+};
--- website/src/pages/index.tsx
+++ website/src/pages/index.tsx
@@ -3,6 +3,7 @@ import { Fragment } from 'react';
 import { jsx } from '@emotion/core';
 import { graphql, PageProps } from 'gatsby';
 import Helmet from 'react-helmet';
+import { Stack } from '@nathsimpson/box';
 
 import {
   Header,
@@ -13,88 +14,23 @@ import {
   Divider,
   Dribbble
 } from '../components';
-import { useTheme } from '@nathsimpson/theme';
-import { Heading, Text } from '@nathsimpson/typography';
-import { Stack } from '@nathsimpson/box';
-import { TextLink } from '@nathsimpson/text-link';
+import { Hero } from '../components/Hero';
 
-const Hero = () => {
-  return (
-    <div
-      css={{
-        padding: '128px 0px',
-        width: '100%'
-      }}
-    >
-      <div
-        css={{
-          maxWidth: '1300px',
-          padding: '0 24px',
-          margin: '0 auto',
-          boxSizing: 'border-box',
-          display: 'flex',
-          alignItems: 'start',
-          justifyContent: 'center',
-          flexDirection: 'column',
-          textAlign: 'left'
-        }}
-      >
-        <Helmet>
-          <meta
-            name="description"
-            content="I am a UI Designer, Frontend Developer, and aspiring entrepreneur, passionate about building ideas from concept to prototype."
-          />
-        </Helmet>
-        <Stack gap="medium" css={{ maxWidth: 800 }}>
-          <Avatar />
-          <Heading level={1}>G'day, I'm Nath 👋</Heading>
-          <Text as="p" size="large">
-            I'm a UI Designer &amp; Developer at{' '}
-            <TextLink href="https://thinkmill.com.au">Thinkmill</TextLink> in
-            Sydney, Australia. I'm passionate about Design Systems, and building
-            products from concept to delivery.
-          </Text>
-        </Stack>
-      </div>
-    </div>
-  );
-};
-
-const Avatar = () => {
-  const { colors } = useTheme();
-  return (
-    <img
-      src="https://files.nathansimpson.design/avatar.jpeg"
-      alt="Nathan is smiling while sitting in a Ford Mustang."
-      css={{
-        width: 100,
-        height: 100,
-        borderRadius: 50,
-        backgroundColor: colors.text.default
-      }}
-    />
-  );
-};
-
-const App = ({ data }: PageProps) => (
+const HomePage = ({ data }: PageProps) => (
   <Fragment>
+    <Helmet>
+      <meta
+        name="description"
+        content="I am a UI Designer, Frontend Developer, and aspiring entrepreneur, passionate about building ideas from concept to prototype."
+      />
+    </Helmet>
     <Header hasDivider={false} />
     <Hero />
-
     <Container>
       <Stack gap="xxxlarge" marginY="xxxlarge" alignItems="center">
         <Portfolio data={data} />
-
-        <Divider />
-
         <Development />
-
-        <Divider />
-
         <Dribbble />
-
-        <Divider />
-
         <Talks data={data} />
       </Stack>
     </Container>
@@ -123,4 +59,4 @@ export const portfolioQuery = graphql`
   }
 `;
 
-export default App;
+export default HomePage;

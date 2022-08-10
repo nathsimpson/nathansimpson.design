a35c572a291bffae857a0cdd512cc1a7d66f0702
Nathan Simpson <hello@nathansimpson.design>
2022-08-10 23:14:21 +1000

    pointing the workspace

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,6 +2,7 @@ module.exports = {
   extends: [
     'eslint:recommended',
     'plugin:@typescript-eslint/recommended',
+    'next/core-web-vitals',
     'plugin:react/recommended',
     'prettier'
   ],

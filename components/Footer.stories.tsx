0662372fc24409a39e4ba2dfbc6ff0c69a4d7030
Nathan Simpson <hello@nathansimpson.design>
2023-04-12 12:55:24 +1000

    Build to next public dir

---
+++ components/Footer.stories.tsx
@@ -0,0 +1,11 @@
+import { Footer } from './Footer';
+import type { Meta } from '@storybook/react';
+
+const meta: Meta = {
+  title: 'Footer',
+  component: Footer
+};
+
+export default meta;
+
+export const Basic = () => <Footer />;

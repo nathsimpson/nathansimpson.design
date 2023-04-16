5abd77a9b0bcc4b6c41265f4dc4a6261ac6c5f8a
Nathan Simpson <hello@nathansimpson.design>
2023-04-16 21:45:27 +1000

    Contact content

--- pages/index.tsx
+++ pages/index.tsx
@@ -7,9 +7,9 @@ import { Portfolio } from '../components/Portfolio';
 import { Container } from '../components/Container';
 import { Development } from '../components/Development';
 import { Dribbble } from '../components/Dribbble';
-import { Contact } from '../components/Contact';
 import { getAllProjects } from '../lib/projects';
 import type { Project } from '../lib/projects';
+import { Prose } from 'components/design-system/prose';
 
 type HomePageProps = {
   allProjects: Project[];
@@ -21,11 +21,25 @@ const Home: NextPage<HomePageProps> = ({ allProjects }) => {
       <Header />
       <Hero />
       <Container>
-        <Stack gap="xxxlarge" marginBottom="xxxlarge" alignItems="center">
+        <Stack gap="xxxlarge" marginBottom="xxxlarge">
           <Portfolio projects={allProjects} />
           <Development />
           <Dribbble />
-          <Contact />
+
+          <Prose>
+            <h2>Contact Me</h2>
+            <p>
+              Would you like to get in touch? Feel free to message me through{' '}
+              <a href="https://twitter.com/nathjsimpson" target="_blank">
+                Twitter
+              </a>{' '}
+              or{' '}
+              <a href="https://www.linkedin.com/in/nathan-simpson-71512a75/?trk">
+                LinkedIn
+              </a>
+              .
+            </p>
+          </Prose>
         </Stack>
       </Container>
     </Fragment>

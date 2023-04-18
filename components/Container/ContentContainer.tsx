d0e5b7e14700d2de7edf675ea94689fd2681ba9f
Nathan Simpson <hello@nathansimpson.design>
2023-04-18 18:36:13 +1000

    Fix issue with container overflow in Containers

--- components/Container/ContentContainer.tsx
+++ components/Container/ContentContainer.tsx
@@ -1,19 +1,20 @@
+import { Box, Flex } from 'components/ui/box';
 import { ReactNode } from 'react';
-import { maxWidth, spacing } from '../../lib/theme';
+import { maxWidth } from '../../lib/theme';
 
 /** This is a container component that is used to constrain the width of written post content */
 export const ContentContainer = ({ children }: { children: ReactNode }) => {
   return (
-    <div
-      css={{
-        maxWidth: maxWidth.content,
-        margin: '0 auto',
-        paddingLeft: spacing.xlarge,
-        paddingRight: spacing.xlarge,
-        width: '100%'
-      }}
-    >
-      {children}
-    </div>
+    <Flex justifyContent="center">
+      <Box
+        paddingX="xlarge"
+        width="100%"
+        css={{
+          maxWidth: maxWidth.content
+        }}
+      >
+        {children}
+      </Box>
+    </Flex>
   );
 };
--- components/Container/PageContainer.tsx
+++ components/Container/PageContainer.tsx
@@ -1,26 +1,24 @@
+import { Box, Flex } from 'components/ui/box';
 import { PropsWithChildren, ElementType } from 'react';
-import { spacing, maxWidth as maxWidthTokens } from '../../lib/theme';
+import { spacing, maxWidth } from '../../lib/theme';
 
 type ContainerProps = PropsWithChildren<{
   as?: ElementType;
 }>;
 
 // This is a container component that is used to constrain the width of a page
-export const PageContainer = ({
-  as: Tag = 'div',
-  children
-}: ContainerProps) => {
+export const PageContainer = ({ as = 'div', children }: ContainerProps) => {
   return (
-    <Tag
-      css={{
-        width: '100%',
-        maxWidth: maxWidthTokens.page,
-        paddingLeft: spacing.medium,
-        paddingRight: spacing.medium,
-        margin: '0 auto'
-      }}
-    >
-      {children}
-    </Tag>
+    <Flex as={as} justifyContent="center">
+      <Box
+        paddingX="medium"
+        width="100%"
+        css={{
+          maxWidth: maxWidth.page
+        }}
+      >
+        {children}
+      </Box>
+    </Flex>
   );
 };

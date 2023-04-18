dae9017a1e8b45032750e239bd1443b48341d825
Nathan Simpson <hello@nathansimpson.design>
2023-04-18 22:34:03 +1000

    Fix issue with container overflow in Containers (#122)
    * Fix issue with container overflow in Containers
    
    * Simplify HomeHero

--- components/Home/HomeHero.tsx
+++ components/Home/HomeHero.tsx
@@ -1,4 +1,4 @@
-import { Box, Stack } from '../ui/box';
+import { Flex, Stack } from '../ui/box';
 import { TextLink } from '../TextLink';
 import { useTheme } from '../../lib/theme';
 import { Heading, Text } from '../ui/typography';
@@ -7,45 +7,43 @@ export const HomeHero = () => {
   const { maxWidth } = useTheme();
 
   return (
-    <Box width="100%" paddingX="medium">
-      <div
+    <Flex
+      alignItems="flex-start"
+      justifyContent="center"
+      flexDirection="column"
+    >
+      <Stack
+        gap="medium"
+        paddingY="xxxlarge"
+        width="100%"
         css={{
-          maxWidth: maxWidth.page,
-          padding: '128px 0px',
-          margin: '0 auto',
-          boxSizing: 'border-box',
-          display: 'flex',
-          alignItems: 'start',
-          justifyContent: 'center',
-          flexDirection: 'column',
-          textAlign: 'left'
+          maxWidth: maxWidth.content
         }}
       >
-        <Stack gap="medium" css={{ maxWidth: maxWidth.content }}>
-          <Avatar />
-          <Heading level={1}>G'day, I'm Nath 👋</Heading>
-          <Text as="p" size="large">
-            I'm a UI Designer &amp; Developer at{' '}
-            <TextLink href="https://thinkmill.com.au">Thinkmill</TextLink> in
-            Sydney, Australia. I'm passionate about Design Systems, and building
-            products from concept to delivery.
-          </Text>
-        </Stack>
-      </div>
-    </Box>
+        <Avatar />
+        <Heading level={1}>G'day, I'm Nath 👋</Heading>
+        <Text as="p" size="large">
+          I'm a UI Designer &amp; Developer at{' '}
+          <TextLink href="https://thinkmill.com.au">Thinkmill</TextLink> in
+          Sydney, Australia. I'm passionate about Design Systems, and building
+          products from concept to delivery.
+        </Text>
+      </Stack>
+    </Flex>
   );
 };
 
 const Avatar = () => {
   const { colors } = useTheme();
+  const size = '6rem';
   return (
     <img
       src="https://files.nathansimpson.design/avatar.jpeg"
       alt="Nathan is smiling while sitting in a Ford Mustang."
       css={{
-        width: 100,
-        height: 100,
-        borderRadius: 50,
+        width: size,
+        height: size,
+        borderRadius: size,
         backgroundColor: colors.text.default
       }}
     />
